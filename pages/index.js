3d5a2ad8117edcb9a85a3d2078c2be9f5047b328
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-03-14 23:07:56 +0300

    Profile photo added to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home Page
+      Welcome to Next.js! <Image src="/images/profile.jpg" width={200} height={200} />
     </div>
   );
 }

b89177dd9982439ff824334497e88d8c54b16e5b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-03-18 11:16:16 +0300

    Authentication worked with supsabase auth

--- pages/account/signup.js
+++ pages/account/signup.js
@@ -5,8 +5,14 @@ import {
   Text,
   Button,
   Container,
+  Flex,
+  Paper,
+  Grid,
+  Box,
+  TextInput,
   Input,
   Group,
+  Image,
 } from "@mantine/core";
 import { createApi  } from "@reduxjs/toolkit/query";
 import { createClient } from "@supabase/supabase-js";
@@ -30,32 +36,105 @@ const Signup = () => {
 
   return (
     <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "dark",
-        fontFamily: "Roboto",
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+      colorScheme: "light",
+      fontFamily: "Roboto",
+      components: {
+        TextInput: {
+          defaultProps: {
+            radius: "sm",
+            w: {
+              base: "100%",
+              sm: "100%",
+              lg: "100%",
+            },
+          },
+        },
+        Button: {
+          defaultProps: {
+            radius: "sm",
+            w: {
+              base: "100%",
+              sm: "100%",
+              lg: "100%",
+            },
+          },
+        },
+        Box: {
+          defaultProps: {
+            radius: "sm",
+            w: {
+              base: "100%",
+              sm: "100%",
+              lg: "100%",
+            },
+          },
+        },
+      },
+    }}
+  >
+    <Flex
+      direction={{ base: "column", sm: "row" }}
+      gap={{ base: "sm", sm: "lg" }}
+      justify={{ sm: "center" }}
+      align={{ sm: "center" }}
+      w={{
+        base: "100%",
+        sm: "100%",
+        lg: "90%",
       }}
     >
-      <Container size="md" padding="xl" align="center">
-        <Text size="xl" weight="bold" color="green">
-          Sign up
+      <Box>
+        <Image src="/Images/Login.svg" pt={60} fit="cover" />
+      </Box>
+      <Box bg="white" px={{ base: "10%", sm: "10%", lg: "0%" }}>
+        <Text
+          size="xl"
+          weight="bold"
+          color="black"
+          td="underline"
+          my="10%"
+          mx="auto"
+          align="center"
+        >
+          Signup
         </Text>
-        <Input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <Input
+
+        <TextInput
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          mx="auto"
+          mb="lg"
+        />
+        <TextInput
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+          my="5%"
+          mx="auto"
         />
-        <Button color="red" mt="lg" variant="filled" 
-          onClick={handleSignup}
-        >
-          Register
-        </Button>
-        <Group position="center">
+        <Group position="center" mt="lg" color="black">
+          <Button
+            href="/account/dashboard"
+            color="orange"
+            mt="lg"
+            mx="auto"
+            align="center"
+            w={500}
+            variant="filled"
+            onClick={handleSignup}
+          >
+            Sign Up
+          </Button>
+        </Group>
+        <Group position="center" mt="lg" color="black">
+          Already have an account?
           <Link href="/account/login">Login</Link>
         </Group>
-      </Container>
-    </MantineProvider>
+      </Box>
+    </Flex>
+  </MantineProvider>
   );
 };
 
